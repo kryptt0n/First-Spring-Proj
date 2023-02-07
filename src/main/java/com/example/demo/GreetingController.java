c0fc034cdc9bd478fc5bbf3b0362497c90a4ef63
kryptt0n <vitaliksuhinin99@gmail.com>
2023-02-07 19:41:09 +0300

    first integration with jdbc

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,7 +14,20 @@ public class GreetingController {
     private final AtomicInteger counter = new AtomicInteger();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world!") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world!") String name) throws SQLException {
+        String curName = "";
+        String sqlQuery = "select * from users where id = %d";
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(String.format(sqlQuery, counter.incrementAndGet()))) {
+
+            resultSet.next();
+
+            curName = resultSet.getString("name");
+
+
+        }
+
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, curName));
     }
 }
