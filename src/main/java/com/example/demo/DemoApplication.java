9019023b750f1cafb9a4c4834dd09b63f790df49
Vitaly <vitaliksuhinin99@gmail.com>
2023-03-23 23:03:29 +0300

    RESOLVED PROBLEM WITH DIgit add .

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,26 +1,19 @@
 package com.example.demo;
 
 import com.example.beans.PCSetup;
+import com.example.config.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com", exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan({"com.example.database", "com.example.demo", "com.example.beans"})
-@EntityScan("com.example.database")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		String path = "/Users/vitaly/Desktop/Java projects/FirstSpringProj/src/main/resources/account-bean-config.xml";
-
-		ApplicationContext context = new FileSystemXmlApplicationContext(path);
-		PCSetup setup = context.getBean(PCSetup.class);
-		System.out.println(setup);
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
 	}
 
 }
